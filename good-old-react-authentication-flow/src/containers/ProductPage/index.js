6f35c149828b8a21aa9c31e28f38a0db8f5655fb
cyril lopez <cyril.lpz@gmail.com>
2018-03-07 12:22:07 +0100

    Display products

--- good-old-react-authentication-flow/src/containers/ProductPage/index.js
+++ good-old-react-authentication-flow/src/containers/ProductPage/index.js
@@ -7,6 +7,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import Table from '../../components/Table';
 // Utils
 import request from '../../utils/request';
 
@@ -19,13 +20,28 @@ class ProductPage extends React.Component {
     const requestURL = 'http://localhost:1337/product';
 
     const products = await request(requestURL, { method: 'GET' });
-    console.log('p', products);
+    this.setState({ products });
   }
 
   render() {
     return (
       <div className="productPageWrapper">
-        PropTypes
+        <div className="container">
+          <div className="row">
+            <div className="col-md-4">
+              <h1>Products</h1>
+            </div>
+            <div className="col-md-4 offset-md-4 productPageLink">
+              Create a Product
+            </div>
+          </div>
+          <div className="row">
+            <Table
+              data={this.state.products}
+              headers={['id', 'name', 'pictures', '']}
+            />
+          </div>
+        </div>
       </div>
     );
   }
