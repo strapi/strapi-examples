cf994faa5f2b3c03ef6dd8adbab062d3f4a368b6
LeLam <lminhlam@gmail.com>
2018-09-20 19:48:34 +0700

    Update gatsby-config.js

--- gatsby-strapi-tutorial/blog/gatsby-config.js
+++ gatsby-strapi-tutorial/blog/gatsby-config.js
@@ -7,7 +7,7 @@ module.exports = {
     {
       resolve: `gatsby-source-strapi`,
       options: {
-        apiURL: `http://localhost:1337`,
+        apiURL: `https://howto-stackaz.herokuapp.com`,
         contentTypes: [ // List of the Content Types you want to be able to request from Gatsby.
           `article`,
           `user`
